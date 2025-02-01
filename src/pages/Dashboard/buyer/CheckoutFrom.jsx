c64e3708c586a9c9b3661fa6a22eee7025979c09
abdulaziz1812 <abdulaziz.abdulmannan@gmail.com>
2025-02-01 16:34:27 +0600

    added payment history

--- src/pages/Dashboard/buyer/CheckoutFrom.jsx
+++ src/pages/Dashboard/buyer/CheckoutFrom.jsx
@@ -1,106 +1,122 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import React, { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hook/useAxiosSecure";
-import { useLoaderData, useLocation } from "react-router-dom";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../../hook/useAuth";
 import Swal from "sweetalert2";
+import useCoin from "../../../hook/useCoin";
 
 const CheckoutFrom = () => {
-    const [error, setError]= useState()
-    const [clientSecret, setClientSecret] = useState('')
-    const stripe = useStripe()
-    const elements= useElements()
-    const axiosSecure = useAxiosSecure();
-    const location =useLocation()
-    const {user} =useAuth()
-    const pkgPrice = location.state?.price;
-    const [transactionId, setTransactionId]= useState('')
-    
+  const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+  const axiosSecure = useAxiosSecure();
+  const location = useLocation();
+  const { user: currentUser } = useAuth();
+  const email= currentUser.email
+  const pkgPrice = location.state?.price;
+  const [transactionId, setTransactionId] = useState("");
+ const navigate =useNavigate()
+  const { user, refetch } = useCoin(email);
+  const coin = user?.coin;
 
-    useEffect(()=>{
-        if(pkgPrice>0){
-            axiosSecure.post('/create-payment-intent',{price: pkgPrice})
-        .then(res =>{
-            console.log(res.data.clientSecret);
-            setClientSecret(res.data.clientSecret)
-        })
-        }
-    },[axiosSecure, pkgPrice])
+  useEffect(() => {
+    if (pkgPrice > 0) {
+      axiosSecure
+        .post("/create-payment-intent", { price: pkgPrice })
+        .then((res) => {
+          console.log(res.data.clientSecret);
+          setClientSecret(res.data.clientSecret);
+        });
+    }
+  }, [axiosSecure, pkgPrice]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(!stripe || !elements){
-        return
+    if (!stripe || !elements) {
+      return;
     }
 
-    const card = elements.getElement(CardElement)
+    const card = elements.getElement(CardElement);
 
-    if(card === null){
-        return
+    if (card === null) {
+      return;
     }
 
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card
-    })
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
 
-    if(error){
-        console.log('payment',error );
-        setError(error.message)
-
-    } else{
-        console.log("payment method",paymentMethod);
-        setError('')
+    if (error) {
+      console.log("payment", error);
+      setError(error.message);
+    } else {
+      console.log("payment method", paymentMethod);
+      setError("");
     }
-    
-    const {paymentIntent, error : confirmError} = await stripe.
-    confirmCardPayment(clientSecret,{
-        payment_method:{
-            card: card,
-            billing_details:{
-                email: user?.email || 'anonymous',
-                name: user?.displayName || 'anonymous'
-            }
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: email || "anonymous",
+            name: currentUser?.displayName || "anonymous",
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log("confirm error");
+    } else {
+      console.log("payment intent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        console.log("transaction ID", paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+
+        const payment = {
+          transactionId: paymentIntent.id,
+          email: email,
+          price: pkgPrice,
+          date: new Date(),
+        };
+        const res = await axiosSecure.post("/payments", payment);
+        console.log("payment res", res.data);
+        if (res.data?.insertedId) {
+          
+console.log(pkgPrice);
+          let newCoins = 0;
+          if (pkgPrice === 1) newCoins = 10;
+          if (pkgPrice === 10) newCoins = 150;
+          if (pkgPrice === 20) newCoins = 500;
+          if (pkgPrice === 35) newCoins = 1000;
+
+          const updatedCoins = coin + newCoins; // Calculate before state update
+
+          await axiosSecure.patch(`/user/${email}`, { coin: updatedCoins });
+
+          Swal.fire({
+            icon: "success",
+            title: `Your payment has done successfully & added ${newCoins} Coins`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+
+          refetch()
+          navigate('/dashboard/buyer/payment-history')
         }
-    })
-
-    if(confirmError){
-        console.log('confirm error');
+      }
     }
-    else{
-        console.log('payment intent', paymentIntent);
-        if(paymentIntent.status === 'succeeded'){
-            console.log('transaction ID', paymentIntent.id);
-            setTransactionId(paymentIntent.id)
-
-            const payment = {
-                transactionId: paymentIntent.id,
-                email: user.email,
-                price: pkgPrice,
-                date: new Date()
-                
-            }
-            const res= await axiosSecure.post('/payments',payment)
-            console.log("payment res",res.data);
-            if(res.data?.insertedId){
-                Swal.fire({
-                    
-                    icon: "success",
-                    title: "Your payment has done successfully",
-                    showConfirmButton: false,
-                    timer: 1500
-                  });
-            }
-        }
-    }
-
   };
   return (
     <div>
-      <form 
-      className="p-10 min-w-2xl m-8 rounded-2xl shadow-2xl"
-      onSubmit={handleSubmit}>
+      <form
+        className="p-10 min-w-2xl m-8 rounded-2xl shadow-2xl"
+        onSubmit={handleSubmit}
+      >
         <CardElement
           options={{
             style: {
@@ -117,13 +133,19 @@ const CheckoutFrom = () => {
             },
           }}
         />
-        <button 
-        className="btn btn-success mt-5"
-        type="submit" disabled={!stripe || !clientSecret}>
+        <button
+          className="btn btn-success mt-5"
+          type="submit"
+          disabled={!stripe || !clientSecret}
+        >
           Pay
         </button>
         <p className="text-red-500">{error}</p>
-        {transactionId && <p className="text-green-600">Your transaction id: {transactionId} </p>}
+        {transactionId && (
+          <p className="text-green-600">
+            Your transaction id: {transactionId}{" "}
+          </p>
+        )}
       </form>
     </div>
   );
--- src/pages/Dashboard/buyer/PaymentHistory.jsx
+++ src/pages/Dashboard/buyer/PaymentHistory.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import useAuth from "../../../hook/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hook/useAxiosSecure";
 
 const PaymentHistory = () => {
-    return (
-        <div>
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: payments =[]} = useQuery({
+    queryKey: ["payments", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user.email}`);
+      return res.data;
+    },
+  });
+
+  const date = (addedDate) => {
+    const newDate = new Date(addedDate);
+    return newDate.toLocaleString("en-gb", {
+      day: "2-digit",
+      year: "numeric",
+      month: "short",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+  };
+  return (
+    <div className="m-10">
+      <div className="overflow-x-auto p-6 rounded shadow-2xl">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr className="text-center">
+              <th>Sl No</th>
+              <th>Price</th>
+              <th>Transaction Id</th>
+              <th>Date of Transaction</th> 
+            </tr>
+          </thead>
+          <tbody>
+            {payments.map((payment, index) =><tr
+            key={payment._id}
+            >
+              <td>{index+1}</td>
+              <td>$ {payment.price}</td>
+              <td>{payment.transactionId}</td>
+              <td>{date(payment.date)}</td>
+            </tr>
+            )}
             
-        </div>
-    );
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentHistory;
